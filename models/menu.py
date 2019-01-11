8266408f4f087366712999883591c55295aa843a
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-10 21:01:59 -0800

    more bugs

--- models/menu.py
+++ models/menu.py
@@ -7,7 +7,7 @@ class MenuModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
-    items = db.relationship('MenuModel', lazy='dynamic')
+    items = db.relationship('ItemModel', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
