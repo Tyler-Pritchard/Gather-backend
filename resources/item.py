699c859014f29f153c6a806f51cf50fde63dae4b
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-15 23:51:38 -0800

    stripe added

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
+from flask import request
+import stripe
 
 
 class Item(Resource):
