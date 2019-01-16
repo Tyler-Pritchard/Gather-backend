d32925fefcf001be932e8053fdaaa6be6c9e5747
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-16 09:03:26 -0800

    ditto

--- resources/orderItem.py
+++ resources/orderItem.py
@@ -33,6 +33,7 @@ class OrderItem(Resource):
 
     def post(self, name, price):
         if OrderItemModel.find_by_name(name):
+            # switch to quantity ++
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = OrderItem.parser.parse_args()
