f90535cfff3240e55183c20be4d0b87d689477d5
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-01-17 14:04:49 -0800

    removed auth token

--- resources/_twilio.py
+++ resources/_twilio.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 import twilio
 
-# Your Account SID from twilio.com/console
-account_sid = "AC7e7637f7467d96072babdbafc41d8509"
+# Your Test Account SID from twilio.com/console
+account_sid = "ACe23db114a4d0ccce327eeeba4e9fad2f"
 # Your Auth Token from twilio.com/console
-auth_token = "d4a16cbf919c613e198c392b1ee8f609"
+auth_token = "********************"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+15209095372",
+    to="+1520909",
     from_="+15206399758",
     body="Hello, Mansoor my name is Python!")
 
