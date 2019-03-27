01b768d2c73ff02d3b4a3303c1123dd59be61c84
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-03-27 16:46:29 -0700

    refactor user errors to constants

--- resources/user.py
+++ resources/user.py
@@ -1,22 +1,26 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
+BLANK_ERROR = "'{}' cannot be blank."
+USER_ALREADY_EXISTS = "A user with that username already exists."
+USER_CREATED_SUCCESSFULLY = "User created successfully."
+
 
 class UserRegister(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('username', type=str, required=True,
-                        help="This field cannot be blank.")
+                        help=BLANK_ERROR)
     parser.add_argument('password', type=str, required=True,
-                        help="This field cannot be blank.")
+                        help=BLANK_ERROR)
 
     def post(self):
         data = UserRegister.parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
-            return {"message": "A user with that username already exists"}, 400
+            return {"message": USER_ALREADY_EXISTS}, 400
 
         user = UserModel(**data)
         user.save_to_db()
 
-        return {"message": "User created successfully."}, 201
+        return {"message": USER_CREATED_SUCCESSFULLY}, 201
