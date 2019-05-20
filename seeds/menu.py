d992dbf4665f187724ad84e33aafd185db3fe310
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-05-19 22:45:13 -0700

    typos

--- seeds/menu.py
+++ seeds/menu.py
@@ -3,36 +3,37 @@ menu = [
     {
         "id": 1,
         "name": "breakfast",
-        "url": "/Menus/breakfast",
+        "url": "/Menus/breakfast"
     },
 
     {
         "id": 2,
         "name": "lunch",
-        "url": "/Menus/lunch",
+        "url": "/Menus/lunch"
     },
 
     {
         "id": 3,
         "name": "salads",
-        "url": "/Menus/salads",
+        "url": "/Menus/salads"
     },
 
     {
         "id": 4,
         "name": "tacos",
-        "url": "/Menus/tacos",
+        "url": "/Menus/tacos"
     },
 
     {
         "id": 5,
         "name": "drinks",
-        "url": "/Menus/drinks",
+        "url": "/Menus/drinks"
     },
+
     {
         "id": 6,
         "name": "catering",
-        "url": "/Menus/catering",
+        "url": "/Menus/catering"
     }
 
 ]
