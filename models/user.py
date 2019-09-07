2426247dff2487587292d472a1be773fe6c8a32b
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-09-06 20:28:38 -0700

    add username json to user model

--- models/user.py
+++ models/user.py
@@ -15,6 +15,12 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
     def save_to_db(self) -> None:
         db.session.add(self)
         db.session.commit()
